83f762ee3800f2f8fb5413fa791dfeee3eda6603
prisca2920 <prisca2920@gmail.com>
2024-09-04 09:47:56 -0700

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import { env } from 'process';
-
-const mainRoute = require('./routes/index');
+import mainRoute from './routes/index.js';
 
 const app = express();
 const port = env.PORT || 5000;
+
 app.use(express.json());
 app.use(mainRoute);
-app.listen(port, '127.0.0.1');
+
+app.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
 
 export default app;
