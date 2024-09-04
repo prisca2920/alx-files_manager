9be7fcf1a3cb36595316c62e87bff3d9d658d8ab
prisca2920 <prisca2920@gmail.com>
2024-09-04 09:56:26 -0700

    user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,30 +1,37 @@
-import UtilController from './UtilController';
-import dbClient from '../utils/db';
+import UtilController from './UtilController.js';
+import dbClient from '../utils/db.js';
 
 export default class UsersController {
-  static async postNew(request, response) {
-    const { email, password } = request.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
     if (!email || !password) {
-      response.status(400).json({ error: `Missing ${!email ? 'email' : 'password'}` }).end();
-    } else if (await dbClient.userExists(email)) {
-      response.status(400).json({ error: 'Already exist' }).end();
-    } else {
-      try {
-        const passwordHash = UtilController.SHA1(password);
-        const insert = await dbClient.newUser(email, passwordHash);
-        const { _id } = insert.ops[0];
-        const _email = insert.ops[0].email;
-        response.status(201).json({ id: _id, email: _email }).end();
-      } catch (err) {
-        response.status(400).json({ error: err.message }).end();
-      }
+      return res.status(400).json({ error: `Missing ${!email ? 'email' : 'password'}` });
+    }
+
+    const userExists = await dbClient.userExists(email);
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    try {
+      const hashedPassword = UtilController.SHA1(password);
+      const newUser = await dbClient.newUser(email, hashedPassword);
+      const userId = newUser.ops[0]._id;
+      const userEmail = newUser.ops[0].email;
+
+      return res.status(201).json({ id: userId, email: userEmail });
+    } catch (err) {
+      return res.status(500).json({ error: 'Unable to create user' });
     }
   }
 
-  static async getMe(request, response) {
-    const { usr } = request;
-    delete usr.password;
-    usr.id = usr._id;
-    delete usr._id;
-    response.status(200).json(usr).end();
-  }}
+  static async getMe(req, res) {
+    const user = { ...req.usr };
+    user.id = user._id;
+    delete user._id;
+    delete user.password;
+
+    return res.status(200).json(user);
+  }
+}
